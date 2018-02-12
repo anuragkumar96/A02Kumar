bc1fc6b727e223feb69f5f41d22b5d6e9864a33c
ANURAG KUMAR <35477729+anuragkumar96@users.noreply.github.com>
2018-02-11 21:18:45 -0600

    Add files via upload

--- cal.js
+++ cal.js
@@ -1,9 +1,21 @@
 function calculate(p,q,r)
 {
+    
     p = document.getElementById("p").value;
+    
     n = document.getElementById("n").value;
+   
     r = document.getElementById("r").value;
+   
     result = document.getElementById("result");
     result.innerHTML = "The interest is " + (p*n*r/100);
 }
 
+
+      
+$(document).ready(function(){
+    $("#button").click(function(){
+  $("#result").empty();
+ 
+    });
+});
